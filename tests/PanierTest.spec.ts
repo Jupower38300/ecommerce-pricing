65e19881d03ca288be61828b5f4e9e7b10167c3a
Jupower38300 <julien.legrand038@gmail.com>
2026-01-15 10:42:53 +0100

    Tests verts

--- tests/PanierTest.spec.ts
+++ tests/PanierTest.spec.ts
@@ -5,7 +5,7 @@ describe ('Gestion du panier', () => {
 
     it("Ajout d'un article au panier avec quantité et prix unitaire", () => {
         const panier = new Panier();
-        const article = new Article("Article 1", 100 ,10);
+        const article = new Article("Article 1", 100 ,10, CategorieArticle.ELECTRONIQUE);
         panier.ajouterArticle(article);
         expect(panier.getArticles()).toHaveLength(1);
         expect(panier.getArticles()[0].quantite).toBe(10);
@@ -14,7 +14,7 @@ describe ('Gestion du panier', () => {
 
     it("Suppression d'un article du panier avec quantité et prix unitaire", () => {
         const panier = new Panier();
-        const article = new Article("Article 1", 100 ,10);
+        const article = new Article("Article 1", 100 ,10, CategorieArticle.ELECTRONIQUE);
         panier.ajouterArticle(article);
 
         panier.supprimerArticle(article, 5);
@@ -26,9 +26,9 @@ describe ('Gestion du panier', () => {
 
     it("Ajouter plus du même article incrémente la quantité", () => {
         const panier = new Panier();
-        const article = new Article("Article 1", 100 ,10);
+        const article = new Article("Article 1", 100 ,10, CategorieArticle.ELECTRONIQUE);
         panier.ajouterArticle(article);
-        panier.ajouterArticle(new Article("Article 1", 100 ,5));
+        panier.ajouterArticle(new Article("Article 1", 100 ,5, CategorieArticle.ELECTRONIQUE));
         expect(panier.getArticles()).toHaveLength(1);
         expect(panier.getArticles()[0].quantite).toBe(15);
     });
@@ -43,15 +43,15 @@ describe('Calcul du total brut du panier', () => {
 
   it('Retourne le total brut pour un article seul', () => {
     const panier = new Panier();
-    panier.ajouterArticle(new Article("Article 1", 10, 3)); // 3 x 10€
+    panier.ajouterArticle(new Article("Article 1", 10, 3, CategorieArticle.ELECTRONIQUE)); // 3 x 10€
 
     expect(panier.calculerTotalBrut()).toBe(30);
   });
 
   it('Retourne le total brut pour plusieurs articles', () => {
     const panier = new Panier();
-    panier.ajouterArticle(new Article("Article 1", 10, 2)); // 20€
-    panier.ajouterArticle(new Article("Article 2", 5, 4));  // 20€
+    panier.ajouterArticle(new Article("Article 1", 10, 2, CategorieArticle.ELECTRONIQUE)); // 20€
+    panier.ajouterArticle(new Article("Article 2", 5, 4, CategorieArticle.ELECTRONIQUE);  // 20€
 
     expect(panier.calculerTotalBrut()).toBe(40);
   });
